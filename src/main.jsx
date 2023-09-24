589f279534d99c31f8bccde2fa867eb24039288f
Rahim_Badsa <rahimbadsa723@gmail.com>
2023-09-24 21:15:28 +0600

    created site layout route and route for manage multipage and set outlet

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import router from './Route/Route'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    hello
+    <RouterProvider router={router}> </RouterProvider>
   </React.StrictMode>,
 )
