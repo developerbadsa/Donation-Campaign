1259fda1b76bd24d5fbb182db5e622c6a39d2724
Rahim_Badsa <rahimbadsa723@gmail.com>
2023-09-25 12:05:27 +0600

    added donation section to home page and load data from api

---
+++ src/Pages/Home/Sections/Donation/Donations.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import Donation from "./Donation";
+
+
+const Donations = () => {
+      const [donations, setDonations] = useState([]);
+       
+      useEffect(()=>{
+            fetch('https://raw.githubusercontent.com/developerbadsa/Donation-Campaign/main/public/Donations.json')
+            .then(res=>res.json())
+            .then(data=>setDonations(data))
+      },[])
+
+
+
+      return (
+            <div>
+                  {
+                        donations.map(donation=> <Donation key={}></Donation>)
+                  }
+            </div>
+      );
+};
+
+export default Donations;
--- src/Route/Route.jsx
+++ src/Route/Route.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "../Layout/Layout";
 import Home from "../Pages/Home/Home";
-import Donation from "../Pages/Donation/Donation";
+import Donation from "../Pages/Home/Sections/Donation/Donations";
 import Statistics from "../Pages/Statistics/Statistics";
 
 const router = createBrowserRouter([
