b1e590dcf1535afc799fda4ced9d08cadd4a0312
Rahim_Badsa <rahimbadsa723@gmail.com>
2023-09-26 02:40:55 +0600

    see more button working fine

--- src/Pages/Donation/MainDonation.jsx
+++ src/Pages/Donation/MainDonation.jsx
@@ -6,22 +6,29 @@ const MainDonation = () => {
 
       const getDatas = JSON.parse(localStorage.getItem('donatedData')) || []
       const [ShowBtn, setShowBtn] = useState(0);
+      const [showAll, setShowAll] = useState(4)
 
-     useEffect(()=>{
-       setShowBtn(getDatas.length+1)
-     },[])
+      useEffect(() => {
+            setShowBtn(getDatas.length + 1)
+      }, [])
 
-      
+
       return (
-            <div className="grid grid-cols-2 gap-8">
-                  {
-                       getDatas.length !== 0 ?  getDatas.map((data, inx)=><DonatedSection key={inx} data={data}></DonatedSection> ): <div className="text-xl font-semibold text-red-400 my-20 text-center">No Data Found In Donation</div>
-                  }
+            <>
 
-            {/* {
-                  ShowBtn >= 4 ? 'hello' : '43434'
-           } */}
-            </div>
+                  <div className="grid grid-cols-2 gap-8">
+                        {
+                              getDatas.length !== 0 ? getDatas.slice(0, showAll).map((data, inx) => <DonatedSection key={inx} data={data}></DonatedSection>) : <div className="text-xl font-semibold text-red-400 my-20 text-center">No Data Found In Donation</div>
+                        }
+
+                        
+                  </div>
+                 <div className="text-center my-10 ">
+                 {
+                              ShowBtn >= 4 ? <button onClick={()=>setShowAll(ShowBtn)} className="btn btn-md bg-[#009444] hover:bg-[#117049] text-white">See More</button> : ''
+                        }
+                 </div>
+            </>
       );
 };
 
