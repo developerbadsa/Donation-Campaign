16cb6164d11be82497b93c1162a624e89f77bce5
Rahim_Badsa <rahimbadsa723@gmail.com>
2023-09-25 12:15:18 +0600

    remove test donation blog from hero

--- src/Pages/Home/Sections/Hero/Hero.jsx
+++ src/Pages/Home/Sections/Hero/Hero.jsx
@@ -6,7 +6,6 @@ const Hero = () => {
             <div>
                   <div className="hero h-[450px]" style={{ backgroundImage: 'url(https://github.com/ProgrammingHero1/donation-campaign/blob/main/Resources/Clothing.png?raw=true)' }}>
                         <div className="hero-overlay bg-[#fff] bg-opacity-90"></div>
-                        <Donations></Donations>
                         <div className="hero-content text-center text-neutral-content">
                               <div className="">
                                     <h1 className="mb-5 text-5xl text-black font-bold">I Grow By Helping People In Need</h1>
