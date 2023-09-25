d7b2c01e0d6859dce3e412e03883c41c4cb8a6ec
Rahim_Badsa <rahimbadsa723@gmail.com>
2023-09-25 13:29:36 +0600

    added link to project donation card

--- src/Pages/Home/Sections/Donation/Donation.jsx
+++ src/Pages/Home/Sections/Donation/Donation.jsx
@@ -1,21 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Donation = ({donation}) => {
 
-      const {card_bg_color, category, category_bg_color, picture, text_color, title
+      const {id ,card_bg_color, category, category_bg_color, picture, text_color, title
       } = donation;
 
 
       return (
-            <div style={{background:`${card_bg_color}`}} className={` border rounded-lg`}>
-                  <div>
-                        <img src={picture} alt="" />
+            <Link style={{background:`${card_bg_color}`}} className={` border rounded-lg`} to={`donate/${id}`}>
+                  <img src={picture} className='w-full' alt={title} />
                         <div className='px-5 py-5'>
                               <button style={{background:`${category_bg_color}`, color:`${text_color}`}} className='btn btn-sm my-4 text-sm font-medium'>{category}</button>
                               <h2 style={{color:`${text_color}`}} className='text-xl font-semibold'>{title}</h2>
                         </div>
-                  </div>
-            </div>
+            </Link>
       );
 };
 
