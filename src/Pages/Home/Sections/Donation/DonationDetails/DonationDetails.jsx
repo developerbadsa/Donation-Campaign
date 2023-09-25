d3747b4d7f58f5c0ead52a03e245b574dd7ae238
Rahim_Badsa <rahimbadsa723@gmail.com>
2023-09-26 00:17:16 +0600

    made handle donate data and set usestate this

--- src/Pages/Home/Sections/Donation/DonationDetails/DonationDetails.jsx
+++ src/Pages/Home/Sections/Donation/DonationDetails/DonationDetails.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useLoaderData, useParams } from "react-router-dom";
 
 
@@ -9,15 +10,19 @@ const DonationDetails = () => {
       const matchedData = datas?.find(data => data.id == getId.id);
 
       const { id, picture, text_color, title, donate_amount, description } = matchedData;
-
-      console.log(matchedData)
-
+      const [localstorage, setLocalstorage] = useState([])
+     const handleDonateAmount = ()=>{
+      
+      setLocalstorage([...localstorage, matchedData])
+      console.log(...localstorage)
+     }
+     
       return (
             <div className='rounded py-24' to={`donate/${id}`}>
 
                   <div className="hero relative min-h-[600px] " style={{ background: `url(${picture})`, backgroundRepeat: "no-repeat", backgroundSize: "cover" }}>
                         <div className="bg-black w-full absolute obsolute flex items-center p-10  bottom-0 my-30 bg-opacity-40">
-                              <button style={{ background: `${text_color}` }} className="btn border-0 btn-primary">Donate ${donate_amount}</button>
+                              <button onClick={()=>handleDonateAmount()} style={{ background: `${text_color}` }} className="btn border-0 btn-primary">Donate ${donate_amount}</button>
                         </div>
                   </div>
                   <h2 className="py-6 text-5xl font-bold">{title}</h2>
