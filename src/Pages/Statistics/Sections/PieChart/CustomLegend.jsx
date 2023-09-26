afdb3bbe0076be5d94decfca83ef6f5ec8a47874
Rahim_Badsa <rahimbadsa723@gmail.com>
2023-09-27 01:13:15 +0600

    pie chart created with custon legend for chart

---
+++ src/Pages/Statistics/Sections/PieChart/CustomLegend.jsx
@@ -0,0 +1,19 @@
+const CustomLegendItem = ({ label, color }) => (
+      <div className="flex items-center justify-center">
+            <div className="ml-10">{label}</div>
+            <div className="w-10 rounded-xl mx-4 h-2" style={{ backgroundColor: color }}></div>
+            
+      </div>
+);
+
+
+
+const CustomLegend = ({ data, colors }) => (
+      <div className="flex justify-center">
+            {data.map(([label], index) => (
+                  <CustomLegendItem key={label} label={label} color={colors[index]} />
+            ))}
+      </div>
+);
+
+export default CustomLegend;
--- src/Pages/Statistics/Sections/PieChart/PieChart.jsx
+++ src/Pages/Statistics/Sections/PieChart/PieChart.jsx
@@ -1,24 +1,21 @@
 
 import { Chart } from 'react-google-charts';
+import CustomLegend from './CustomLegend';
 
 const MyPieChart = () => {
   const data = [
-      
     ['Task', 'Hours per Day'],
     ['Your Donation', 10],
     ['Total Donation', 90],
-    
   ];
 
   const options = {
     title: 'My Daily Activities',
     colors: ['#FF444A', '#00C49F'],
-    legend: {
-      alignment: 'center',
-      position: 'bottom',  
-    },
-    
+    legend: 'none',
   };
+
+  const colors = options.colors;
 
   return (
     <div className="pie-chart-container">
@@ -29,6 +26,7 @@ const MyPieChart = () => {
         width="100%"
         height="400px"
       />
+      <CustomLegend data={data.slice(1)} colors={colors} />
     </div>
   );
 };
