2a9d0ec7875e6488863d89279187274a66d697d4
Rahim_Badsa <rahimbadsa723@gmail.com>
2023-09-27 22:45:15 +0600

    in home page, donations components i have made margin botton for large device and make margin for mobile

--- src/Pages/Home/Sections/Donation/Donations.jsx
+++ src/Pages/Home/Sections/Donation/Donations.jsx
@@ -21,7 +21,7 @@ const Donations = ({ Found }) => {
   }, [Found, donations]); 
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 my-10 lg:mb-52">
       {searchedDonations.length > 0
         ? searchedDonations.map((donation) => (
             <Donation key={donation.id} donation={donation} />
