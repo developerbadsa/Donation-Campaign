f8ef27de0fec3fe92f7150d8e298ec873ca4bbd2
Rahim_Badsa <rahimbadsa723@gmail.com>
2023-09-27 22:59:21 +0600

    git error handaling page more dynamic

--- src/ErrorPage/ErrorPage.jsx
+++ src/ErrorPage/ErrorPage.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useRouteError } from 'react-router-dom';
 
 const ErrorPage = () => {
+      const error = useRouteError()
+      console.log(error)
+
       return (
             <div className='text-center my-40'>
-                  <h5 className='text-4xl'>Page Not Found</h5>
+                  <h5 className='text-4xl'>{error.status}! {error.statusText}</h5>
+                  <p className='my-8 text-sm text-red-500'>{error.data}</p>
                   <Link to={'/'}>
                         <button className='btn bg-[#FF444A] text-white my-8'>Go Home</button>
                   </Link>
