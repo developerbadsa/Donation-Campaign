584cf45340e3eca6552ad46c5c095933c06a4fe5
Rahim_Badsa <rahimbadsa723@gmail.com>
2023-09-27 22:35:08 +0600

    logo image was local loaded now this load from url

--- src/assets/Components/Header/Logo/Logo.jsx
+++ src/assets/Components/Header/Logo/Logo.jsx
@@ -4,7 +4,7 @@ const Logo = () => {
       return (
             <div>
                   <a  className="mr-4 block cursor-pointer py-1.5 font-sans text-sm font-normal leading-normal text-inherit antialiased" >
-                        <img src="../../../../../public/logo/Logo.png" alt="" />
+                        <img src="https://github.com/developerbadsa/Donation-Campaign/blob/main/public/logo/Logo.png?raw=true" alt="" />
                   </a>
             </div>
       );
