53185c3c60dda7d17d044274d019e0676226851b
Rahim_Badsa <rahimbadsa723@gmail.com>
2024-01-04 16:07:46 +0600

    updated

--- src/ErrorPage/ErrorPage.jsx
+++ src/ErrorPage/ErrorPage.jsx
@@ -9,7 +9,7 @@ const ErrorPage = () => {
             <div className='text-center my-40'>
                   <h5 className='text-4xl'>{error.status}! {error.statusText}</h5>
                   <p className='my-8 text-sm text-red-500'>{error.data}</p>
-                  <Link to={'/'}>
+                  <Link to='/'>
                         <button className='btn bg-[#FF444A] text-white my-8'>Go Home</button>
                   </Link>
             </div>
